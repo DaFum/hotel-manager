be84f3f3b59954e39c35c27eb014aa006950de09
DaFum <150466734+DaFum@users.noreply.github.com>
2026-08-21 15:54:29 +0200

    Delete debug60.ts

--- debug60.ts
+++
@@ -1,8 +0,0 @@
-import { STARTER_PLANT } from "./src/game/content/1991/plant";
-
-const baseline = STARTER_PLANT.reduce((sum, a) => {
-  // maybe accumulated depreciation or condition applied?
-  return sum + (a.replacementMinor * a.startingCondition / 100);
-}, 0);
-
-console.log("baseline:", baseline);
